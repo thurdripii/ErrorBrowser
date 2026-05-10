bc6ccd0e4b1fe7bff48d11f562394f55ba9627d6
Thurdripii <arthur.augusto.1707@gmail.com>
2026-05-10 15:47:11 -0300

    Implement BrowserUI with URL input and ErrorEngine

--- ui/browser_ui.py
+++ ui/browser_ui.py
@@ -1,1 +1,26 @@
+from PyQt5.QtWidgets import QMainWindow, QLineEdit, QVBoxLayout, QWidget
+from engine.yeetcore.engine import ErrorEngine
 
+class BrowserUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Error Browser")
+
+        self.engine = ErrorEngine()
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.load_url)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.url_bar)
+        layout.addWidget(self.engine.get_view())
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+    def load_url(self):
+        url = self.url_bar.text()
+        self.engine.load(url)
